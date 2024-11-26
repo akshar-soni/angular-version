8ac19a0dccf2c52905afe17c7e3583b9214ed968
akshar-soni <107323065+akshar-soni@users.noreply.github.com>
2024-11-26 19:45:58 +0530

    feat:ui

--- version-change/src/app/app.component.ts
+++ version-change/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+const packageVersion = require('../../package.json').version;
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'version-change';
+  ngOnInit() {
+    console.log(packageVersion);
+  }
 }
